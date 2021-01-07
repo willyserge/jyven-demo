5e9068e0821cafa3750ea139f02472d37ab1c81b
willyserge <willyserge01@gmail.com>
2021-01-07 18:04:20 +0200

    a few changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -23,7 +23,7 @@ class MyApp extends App {
     return (
       <Fragment>
         <Head>
-          <title>Lezada | React Next JS Multipurpose eCommerce Template</title>
+          <title>Jyven</title>
           <link rel="icon" href={process.env.PUBLIC_URL + "/favicon.ico"} />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
